68af313520579334a290ebbdd5c7b69977155b29
Ana Maria Biliciuc <70842937+AnaMariaBiliciuc@users.noreply.github.com>
2022-09-14 16:34:15 +0300

    Add files via upload

---
+++ f. lista float.py
@@ -0,0 +1,8 @@
+def creare_lista() : 
+    n= int(input("Nr de elemente: "))
+    lista_locala=[]
+    for i in range(n) :
+        elem=float(input("Elementul"+str(i)+ "este: "))
+    return lista_locala
+lista1= creare_lista()
+print(lista1)
